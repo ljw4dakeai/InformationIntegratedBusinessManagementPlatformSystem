97799fda1bb825a6b67c8eb8d208c8e8ea9ef65f
ljw4dakeai <2631247954@qq.com>
2023-02-21 10:35:07 +0800

    init

---
+++ src/main/java/com/zoujiahao/informationintegratedbusinessmanagementplatformsystem/InformationIntegratedBusinessManagementPlatformSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zoujiahao.informationintegratedbusinessmanagementplatformsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationIntegratedBusinessManagementPlatformSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformationIntegratedBusinessManagementPlatformSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zoujiahao/informationintegratedbusinessmanagementplatformsystem/InformationIntegratedBusinessManagementPlatformSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoujiahao.informationintegratedbusinessmanagementplatformsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationIntegratedBusinessManagementPlatformSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
